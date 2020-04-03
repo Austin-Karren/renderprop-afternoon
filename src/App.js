b1fc137ac577601d047606124c118e52a5c5aece
Austin-Karren <austin.t.karren@gmail.com>
2020-04-03 14:50:51 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import CurrencyConverter from './Components/CurrencyConverter';
+import CurrencyDisplay from './Components/CurrencyDisplay';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <h2>
+          Render Props
+        </h2>
+        <CurrencyConverter 
+          render={(currencyData, amount) => (
+            // <p>
+            //   US Dollan ${amount.toFixed(2)} - {currencyData.name}{' '}
+            //   {currencyData.symbol}
+            //   {(amount * currencyData.rate).toFixed(2)}
+            // </p>
+            <CurrencyDisplay currencyData={currencyData} amount={amount} />
+          )}
+        />
+      </>
     );
   }
 }
